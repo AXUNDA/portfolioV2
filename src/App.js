9f7fd00bb77023d270b6a6e6c2fc3406fe0dcdf3
AXUNDA <charlesemmanuelazunda@gmail.com>
2023-03-01 09:24:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+import Hero from "./components/Hero"
+import Contact from "./components/Contact"
+import Who from "./components/Who"
+import Works from "./components/Works"
+import styled from "styled-components"
+
+const Container = styled.div`
+height:100vh;
+scroll-snap-type: y mandatory;
+scroll-behavior:smooth;
+overflow-y:auto;
+color:white;
+scrollbar-width:none;
+&::-webkit-scrollbar{
+  display:none
+}
+
+`
+
+function App() {
+  return (
+    <Container>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+
+
+    </Container>
+  );
+}
+
+export default App;
