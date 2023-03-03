0c6744d35e6360ae744d4587592aa19f353a7648
AXUNDA <charlesemmanuelazunda@gmail.com>
2023-03-03 07:54:15 +0100

    ....

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,17 +6,39 @@ import Navbar from './Navbar';
 const Section = styled.div`
 height:100vh;
 display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-between;
 scroll-snap-align:center;`
 
 const Container = styled.div`
 height:100vh;
 scroll-snap-align:center;
+width:1400px;
+display: flex;
+justify-content: space-between;
+
+`
+const Left = styled.div`
+
+
+`
+const Right = styled.div`
+
+
 
 `
 const Hero = () => {
       return (
             <Section>
                   <Navbar />
+                  <Container>
+                        <Left>
+                              <Title>Ideate. Create. Refine</Title>
+                              <Line />
+                        </Left>
+                        <Right></Right>
+                  </Container>
             </Section>
       );
 }
